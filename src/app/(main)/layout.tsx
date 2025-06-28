ffd8bb878c9789dfa02b255b1266622d247dad07
Kisan Kumavat <kisankumavat85@gmail.com>
2025-06-28 19:45:15 +0530

    Tailwind upgrade

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,7 @@ const ChatLayout = (props: Props) => {
   const { children } = props;
 
   return (
-    <div className="grid grid-cols-[auto,_minmax(0,_1fr)] h-full">
+    <div className="grid grid-cols-[auto_minmax(0,1fr)] h-full">
       <Sidebar />
       <div className="p-4">{children}</div>
     </div>
